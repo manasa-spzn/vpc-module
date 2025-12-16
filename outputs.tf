a1b603bfb812d124d0421b9335b0406f5dd34613
Manasa <6manasak@gmail.com>
2025-12-15 20:58:37 -0500

    added subnet ID outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "private_subnet_ids" {
   description = "The IDs of the private subnets"
   value       = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
 }
+
+output "public_subnet_ids" {
+  description = "The IDs of the public subnets"
+  value       = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+}
